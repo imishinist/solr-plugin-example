15fd8e307ad0d02803217c977e8d6faf82fcf13b
Taisuke Miyazaki <imishinist@gmail.com>
2021-05-01 21:33:49 +0900

    add step function

---
+++ src/main/java/org/imishinist/solr/plugins/StepFunction.java
@@ -0,0 +1,63 @@
+package org.imishinist.solr.plugins;
+
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.queries.function.FunctionValues;
+import org.apache.lucene.queries.function.ValueSource;
+import org.apache.lucene.queries.function.docvalues.IntDocValues;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class StepFunction extends ValueSource {
+    protected final ValueSource source;
+    protected final float[] steps;
+    protected final int[] points;
+
+    public StepFunction(ValueSource source, float[] steps, int[] points) {
+        this.source = source;
+        this.steps = steps;
+        this.points = points;
+    }
+
+    protected String name() {
+        return "step";
+    }
+
+    @Override
+    public FunctionValues getValues(Map context, LeafReaderContext readerContext) throws IOException {
+        final FunctionValues s = source.getValues(context, readerContext);
+
+        return new IntDocValues(this) {
+            @Override
+            public int intVal(int doc) throws IOException {
+                float x = s.floatVal(doc);
+
+                if (x < steps[0] || steps[steps.length - 1] <= x)
+                    return 0;
+                int i = Arrays.binarySearch(steps, x);
+                return points[i];
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (StepFunction.class != o.getClass())
+            return false;
+
+        StepFunction other = (StepFunction) o;
+        return this.source.equals(other.source) &&
+                Arrays.equals(this.steps, other.steps);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this.steps) + this.source.hashCode();
+    }
+
+    @Override
+    public String description() {
+        return name() + '(' + Arrays.toString(this.steps) + ')';
+    }
+}
---
+++ src/main/java/org/imishinist/solr/plugins/StepFunctionValueSourceParser.java
@@ -0,0 +1,32 @@
+package org.imishinist.solr.plugins;
+
+import org.apache.lucene.queries.function.ValueSource;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.search.FunctionQParser;
+import org.apache.solr.search.SyntaxError;
+import org.apache.solr.search.ValueSourceParser;
+
+import java.util.ArrayList;
+
+public class StepFunctionValueSourceParser extends ValueSourceParser {
+    @Override
+    public void init(NamedList namedList) {
+    }
+
+    @Override
+    public ValueSource parse(FunctionQParser fp) throws SyntaxError {
+        ValueSource source = fp.parseValueSource();
+        int num = fp.parseInt();
+        final float[] steps = new float[num];
+        for (int i = 0; i < num; i++) {
+            steps[i] = fp.parseFloat();
+        }
+
+        final int[] points = new int[num];
+        for (int i = 0; i < num-1; i++) {
+            points[i] = fp.parseInt();
+        }
+
+        return new StepFunction(source, steps, points);
+    }
+}
